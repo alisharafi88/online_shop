feb735b7b2df3647d38d0fc92aadda1577a5bb4d
alisharafi88 <alisharafi893@gmail.com>
2023-02-19 02:06:00 +0330

    add comment to products

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class Product(models.Model):
@@ -17,3 +18,22 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.pk])
+
+
+class Comment(models.Model):
+    POINT_CHOICES = [
+        ("1", 'very bad'),
+        ("2", 'bad'),
+        ("3", 'normal'),
+        ("4", 'good'),
+        ("5", 'very good'),
+
+    ]
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments',)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments',)
+    text = models.TextField()
+    point = models.CharField(max_length=10, choices=POINT_CHOICES)
+    active = models.BooleanField(default=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
--- products/views.py
+++ products/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Product
-
+from .models import Product, Comment
+from .forms import CommentsForm
 
 class ProductListView(generic.ListView):
     queryset = Product.objects.filter(active=True)
@@ -14,3 +14,18 @@ class ProductDetailView(generic.DetailView):
     model = Product
     template_name = 'products/product_detail.html'
     context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context['comment_form'] = CommentsForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        new_comment = Comment(
+            product=self.get_object(),
+            user=self.request.user,
+            text=self.request.POST.get('text'),
+        )
+        new_comment.save()
+        return self.get(self, request, *args, **kwargs)
