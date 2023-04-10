7cd409d0804ed22f7d83863417ce9e8f280a9d92
alisharafi88 <alisharafi893@gmail.com>
2023-04-11 03:32:24 +0430

    customized all auth templates

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,24 +12,3 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
         fields = ('email', 'username',)
-
-
-class YourLoginForm(LoginForm):
-    def __init__(self, *args, **kwargs):
-        super(YourLoginForm, self).__init__(*args, **kwargs)
-        self.fields['password'].widget = PasswordInput()
-
-        # You don't want the `remember` field?
-        if 'remember' in self.fields.keys():
-            del self.fields['remember']
-
-        helper = FormHelper()
-        helper.form_show_labels = False
-        helper.layout = Layout(
-            Field('login', placeholder = 'Email address'),
-            Field('password', placeholder = 'Password'),
-            FormActions(
-                Submit('submit', 'Log me in to Cornell Forum', css_class = 'btn-primary')
-            ),
-        )
-        self.helper = helper
